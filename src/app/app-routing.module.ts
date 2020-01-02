165d9276a6f691f00117263fc1dcb938b61ab9bb
umair <umair.javaid@tenpearls.com>
2020-01-02 12:56:32 +0500

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'check-out', component: CheckoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavBarComponent,
+    ProductsComponent,
+    ProductCardComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
